8cd4dec093f804a9cf8c3620ac68cca265aa3c31
Md. Mahmudul Hasan Faisal <bdmhfaisal@gmail.com>
2021-06-29 22:58:56 +0600

    usage of sizeof() for calculating allocation of types

--- FirstApp/FirstApp.cpp
+++ FirstApp/FirstApp.cpp
@@ -6,7 +6,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Hi there!";
+    cout << "size of bool\t" << sizeof(bool) << endl;
+    cout << "size of char\t" << sizeof(char) << endl;
+    cout << "size of int\t" << sizeof(int) << endl;
+    cout << "size of unsigned int\t" << sizeof(unsigned int) << endl;
+    cout << "size of long int\t" << sizeof(long long int) << endl;
+    cout << "size of float\t" << sizeof(float) << endl;
+    cout << "size of double\t" << sizeof(float) << endl;
 
     system("pause>0");
 }
