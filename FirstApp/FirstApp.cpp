d8178c505481314b96df89deb9ba7fae554ba8ee
Md. Mahmudul Hasan Faisal <bdmhfaisal@gmail.com>
2021-07-02 22:37:59 +0600

    Max of data types

--- FirstApp/FirstApp.cpp
+++ FirstApp/FirstApp.cpp
@@ -6,13 +6,22 @@ using namespace std;
 
 int main()
 {
-    cout << "size of bool\t" << sizeof(bool) << endl;
+    /*cout << "size of bool\t" << sizeof(bool) << endl;
     cout << "size of char\t" << sizeof(char) << endl;
     cout << "size of int\t" << sizeof(int) << endl;
     cout << "size of unsigned int\t" << sizeof(unsigned int) << endl;
     cout << "size of long int\t" << sizeof(long long int) << endl;
     cout << "size of float\t" << sizeof(float) << endl;
-    cout << "size of double\t" << sizeof(float) << endl;
+    cout << "size of double\t" << sizeof(float) << endl;*/
+    
+    cout << "Max of char\t" << CHAR_MAX << endl;
+    cout << "Max of int\t" << INT32_MAX << endl;
+    cout << "Max of unsigned int\t" << UINT32_MAX << endl;
+    cout << "Max of long\t" << LLONG_MAX << endl;
+    cout << "Max of float\t" << FLT_MAX << endl;
+    cout << "Max of double\t" << DBL_MAX << endl;
+
+
 
     system("pause>0");
 }
