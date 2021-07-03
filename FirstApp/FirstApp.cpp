e772c0e86f167c14a91a2fb856c2eee3bd87ddd8
Md. Mahmudul Hasan Faisal <bdmhfaisal@gmail.com>
2021-07-04 02:39:02 +0600

    usage of conditional if-else

--- FirstApp/FirstApp.cpp
+++ FirstApp/FirstApp.cpp
@@ -6,5 +6,21 @@ using namespace std;
 
 int main()
 {
+    double x, y, z;
+    cout << "Input length of three arms of a triangle:\t";
+    cin >> x >> y >> z;
+    if (x == y && y == z)
+    {
+        cout << "Equilateral";
+    }
+    else if (x != y && x != z && y != z)
+    {
+        cout << "scalene";
+    }
+    else
+    {
+        cout << "isoscales";
+    }
+    cout << endl;
     system("pause>0");
 }
