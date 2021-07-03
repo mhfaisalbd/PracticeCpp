f45729b06322b29b4299f0b0af2d682abd21ade8
Md. Mahmudul Hasan Faisal <bdmhfaisal@gmail.com>
2021-07-03 19:07:39 +0600

    Overflow of dataTypes

--- FirstApp/FirstApp.cpp
+++ FirstApp/FirstApp.cpp
@@ -14,12 +14,26 @@ int main()
     cout << "size of float\t" << sizeof(float) << endl;
     cout << "size of double\t" << sizeof(float) << endl;*/
     
-    cout << "Max of char\t" << CHAR_MAX << endl;
+    /*cout << "Max of char\t" << CHAR_MAX << endl;
     cout << "Max of int\t" << INT32_MAX << endl;
     cout << "Max of unsigned int\t" << UINT32_MAX << endl;
     cout << "Max of long\t" << LLONG_MAX << endl;
     cout << "Max of float\t" << FLT_MAX << endl;
-    cout << "Max of double\t" << DBL_MAX << endl;
+    cout << "Max of double\t" << DBL_MAX << endl;*/
+
+    char ch = CHAR_MAX + 1;
+    int i = INT32_MAX + 1;
+    unsigned int ui = UINT32_MAX + 1;
+    long long lli = LLONG_MAX + 1;
+    float f = FLT_MAX + 1;
+    double d = DBL_MAX + 1;
+
+    cout << "Overflow of char\t" << ch << endl;
+    cout << "Overflow of int\t" << i << endl;
+    cout << "Overflow of unsigned int\t" << ui << endl;
+    cout << "Overflow of long\t" << lli << endl;
+    cout << "Overflow of float\t" << f << endl;
+    cout << "Overflow of double\t" << d << endl;
 
 
 
