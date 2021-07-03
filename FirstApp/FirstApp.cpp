b7b59456b8fd7196057f5fca3afefda9fb12a365
Md. Mahmudul Hasan Faisal <bdmhfaisal@gmail.com>
2021-07-04 02:43:10 +0600

    inline conditional statements

--- FirstApp/FirstApp.cpp
+++ FirstApp/FirstApp.cpp
@@ -9,18 +9,8 @@ int main()
     double x, y, z;
     cout << "Input length of three arms of a triangle:\t";
     cin >> x >> y >> z;
-    if (x == y && y == z)
-    {
-        cout << "Equilateral";
-    }
-    else if (x != y && x != z && y != z)
-    {
-        cout << "scalene";
-    }
-    else
-    {
-        cout << "isoscales";
-    }
-    cout << endl;
+    cout << ((x == y && y == z) ? "Equilateral" :
+    (x != y && x != z && y != z) ?
+    "scalene" : "isoscales") << endl;
     system("pause>0");
 }
