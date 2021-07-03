43a4f7f5104bc38c02e8325575aa2cd830fdc700
Md. Mahmudul Hasan Faisal <bdmhfaisal@gmail.com>
2021-07-04 03:16:40 +0600

    usage of switch case

--- FirstApp/FirstApp.cpp
+++ FirstApp/FirstApp.cpp
@@ -6,10 +6,30 @@ using namespace std;
 
 int main()
 {
-    int p = 6, q = 7;
-    cout << (p != q) << endl;
-    cout << (p == 6 && q == 6) << endl;
-    system("cls");
-    cout << (p == 7 || q == 7) << endl;
+    int month, year;
+    cout << "Input MM yyyy\t";
+    cin >> month >> year;
+
+    cout << "days in month:\t";
+    switch (month)
+    {
+    case 2: ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+        ? cout << 29 : cout << 28; break;
+        case 4:
+        case 6:
+        case 9:
+        case 11: cout << 30; break;
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12: cout << 31; break;
+
+        default:
+            cout << "Invalid Month!"; break;
+    }
+    cout << endl;
     system("pause>0");
 }
